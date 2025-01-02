8b523f237bf0e9bc86b38641c67758bd630542dd
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-02 18:25:25 +0600

    added header basic file structure and also navbar basic file structure done with import navbar in the header component done on 01/02/2025 at 06:25 pm

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const MainLayout = () => {
     return (
         <>
             <div className="w-full h-screen flex flex-col items-center justify-center">
-
+                <Header></Header>
+                <div className="w-full h-screen">
+                    <Outlet></Outlet>
+                </div>
             </div>
         </>
     );
