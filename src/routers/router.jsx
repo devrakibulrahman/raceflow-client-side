83a0cfd23e2c31404672623442bea858aaa8a0b7
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-02 18:16:42 +0600

    added main layout in the route file done on 01/02/2025 at 06:16 pm

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App></App>,
+        element: <MainLayout></MainLayout>,
         children: [
             {
                 path: '/',
