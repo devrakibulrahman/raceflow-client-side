93453aab340097279fdf2e2956a2834176574573
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-02 19:24:46 +0600

    added router provider in the main.jsx component file done on 01/02/2025 at 07:24 pm

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom';
 import './styles/tailwind.css';
+import router from './routers/router';
 import MainLayout from './layouts/MainLayout';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MainLayout></MainLayout>
+    <RouterProvider router={router}>
+      <MainLayout></MainLayout>
+    </RouterProvider>
   </StrictMode>
 );
