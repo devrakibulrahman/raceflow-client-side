7378ae8e6a6e11c045da6402af251aa58550a3fa
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-02 18:49:24 +0600

    navbar logo import done on 01/02/2025 at 06:49 pm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
+import '../styles/custom.css';
+import light from '../assets/raceflow-light.png';
 
 const Navbar = () => {
     return (
         <>
-            <header className="w-full py-6">
+            <header className='w-full py-6'>
+                <div className='w-full'>
+                    <div className='container mx-auto px-4'>
+                        {/* desktop navbar design */}
+                        <div className='w-full flex items-center justify-between'>
+                            <div className='w-auto'>
+                                <img src={light} alt="logo" className='w-[200px] h-full object-cover' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </header>
         </>
     );
