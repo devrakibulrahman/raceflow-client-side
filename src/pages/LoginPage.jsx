e1dc23725adaf6f717c73ef508a0e0842dccbe3c
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-03 16:43:37 +0600

    login page layout done on 01/03/2025 at 04:43 pm

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,20 @@
 const LoginPage = () => {
     return (
         <>
+            <div className="w-full">
+                <div className="container mx-auto px-4">
+                    <div className="w-full min-h-[calc(100vh-176px)] flex items-center justify-center">
+                        <div className="w-full min-h-[600px] grid grid-cols-1 lg:p-10 lg:grid-cols-2">
+                            <div className="w-full h-[350px] bg-black md:min-h-full">
+
+                            </div>
+                            <div className="w-full h-[350px] bg-blue-400 md:min-h-full">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
