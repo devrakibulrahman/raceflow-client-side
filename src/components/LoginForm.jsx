1546252d7a3965698fc8fbe4e787a1164a7c3f15
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-03 21:13:18 +0600

    make a check box with remember me text done on 01/03/2025 at 09:13 pm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -31,23 +31,31 @@ const LoginForm = () => {
                         </div>
                     </div>
                     <div className="w-full">
-                        <div className="w-full mb-3">
-                            <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug">Password</label>
+                        <div className="w-full">
+                            <div className="w-full mb-3">
+                                <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug">Password</label>
+                            </div>
+                            <div className="w-full relative">
+                                <input type={showPassword ? 'text' : 'password'} placeholder="password" name="password" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
+                                <button onClick={handleShowPassword} className="w-auto absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer">
+                                    {
+                                        showPassword
+                                        ?
+                                            <AiFillEyeInvisible className="text-2xl text-[#A1A1AA]"></AiFillEyeInvisible>
+                                        :
+                                            <AiFillEye className="text-2xl text-[#A1A1AA]"></AiFillEye>
+                                    }
+                                </button>
+                            </div>
                         </div>
-                        <div className="w-full relative">
-                            <input type={showPassword ? 'text' : 'password'} placeholder="password" name="password" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
-                            <button onClick={handleShowPassword} className="w-auto absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer">
-                                {
-                                    showPassword
-                                    ?
-                                        <AiFillEyeInvisible className="text-2xl text-[#A1A1AA]"></AiFillEyeInvisible>
-                                    :
-                                        <AiFillEye className="text-2xl text-[#A1A1AA]"></AiFillEye>
-                                }
-                            </button>
+                        <div className="w-full py-2 mt-3">
+                            <div className="w-auto flex items-center gap-2">
+                                <input type="checkbox" />
+                                <p className="font-roboto text-head-charleston-green text-base font-medium ">Remember Me</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="w-full pt-5">
+                    <div className="w-full">
                         <div className="w-full flex items-center justify-between gap-x-3">
                             <div className="w-full border-b border-head-charleston-green transition ease-linear duration-200"></div>
                             <p className="font-roboto text-sm text-head-charleston-green font-bold uppercase">Or</p>
