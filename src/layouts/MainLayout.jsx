c2ac2dea93c47d3ec2426f05cf69ca54b2d6ab03
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-03 16:16:24 +0600

    location hooks declare in the main layout page done on 01/03/2025 at 04:16 pm

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import '../styles/custom.css';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
+
+    // hooks declare here ---->
+    const location = useLocation();
+
     return (
         <>
             <div className="w-full h-screen flex flex-col items-center">
