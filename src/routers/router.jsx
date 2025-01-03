f669a6e95d3f62aa94665d84b382bee1ce7d7591
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-03 15:58:55 +0600

    import the auth component file in the router.jsx file done on 01/03/2025 at 03:58 pm

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
+import Auth from "../pages/auth/Auth";
 
 const router = createBrowserRouter([
     {
