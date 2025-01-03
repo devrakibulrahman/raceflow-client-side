4c06e8270935499d3ba69adf7fd213d29f4a1a89
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-04 00:59:46 +0600

    heading text added in the contact page done on 01/04/2025 at 12:59 am

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,8 +2,14 @@
 const ContactPage = () => {
     return (
         <>
-            <div className="w-full min-h-[500px] bg-contact bg-center bg-cover bg-no-repeat">
-
+            <div className="w-full min-h-[500px] bg-contact bg-center bg-cover bg-no-repeat flex items-center justify-center">
+                <div className="w-full">
+                    <div className="container mx-auto px-4">
+                        <div className="w-full text-center">
+                            <h1 className="font-roboto text-[40px] font-bold text-white uppercase md:text-[50px] xl:text-[70px]">Contact Us</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
