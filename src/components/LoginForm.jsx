f3f7672590e0e1b501f81227dfbd54dc112cdf8b
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-03 20:56:17 +0600

    added event function in the show password button done on 01/03/2025 at 08:56 pm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -31,7 +31,7 @@ const LoginForm = () => {
                         </div>
                         <div className="w-full relative">
                             <input type="text" placeholder="password" name="password" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
-                            <button className="w-auto absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer">
+                            <button onClick={handleShowPassword} className="w-auto absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer">
                                 <AiFillEye className="text-2xl text-[#A1A1AA]"></AiFillEye>
                             </button>
                         </div>
