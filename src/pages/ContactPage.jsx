73cd944067020bfd2a93a8cfeaf86ee99283b82b
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-04 00:45:48 +0600

    image added in the contact page done on 01/04/2025 at 12:45 am

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,9 @@
 const ContactPage = () => {
     return (
         <>
-            
+            <div className="w-full min-h-[500px] bg-contact bg-center bg-cover bg-no-repeat">
+
+            </div>
         </>
     );
 };
