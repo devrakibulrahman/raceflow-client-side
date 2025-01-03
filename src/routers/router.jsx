b0185807f332de73671b2e04a590b2898c471c43
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-04 00:35:30 +0600

    added new contact route in the router.jsx file done on 01/04/2025 at 12:35 am

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Auth from "../pages/auth/Auth";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
+import ContactPage from "../pages/ContactPage";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/contact',
+                element: <ContactPage></ContactPage>
             },
             {
                 path: '/auth',
