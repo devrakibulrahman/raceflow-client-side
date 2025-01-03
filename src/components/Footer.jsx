0294b85f2c3f59b9178951ab8f12cdbe27bca119
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-03 11:22:26 +0600

    footer link done on 01/03/2025 at 11:22 am

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <footer className="w-full min-h-[200px] py-10 bg-black/5 md:py-[80px]">
                 <div className="w-full">
                     <div className="container mx-auto px-4">
-                        <div className="w-full grid grid-cols-1 gap-y-5 md:grid-cols-2 xl:grid-cols-3">
+                        <div className="w-full grid grid-cols-2 gap-y-5 md:grid-cols-2 xl:grid-cols-3">
                             <div className="w-full min-h-[140px] flex flex-col items-start justify-between col-span-2 xl:col-span-1 md:min-h-[150px]">
                                 <div className='w-auto'>
                                     <img src={light} alt="logo" className='w-[200px] h-full object-cover md:w-[270px]' />
@@ -39,11 +39,40 @@ const Footer = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-full min-h-[300px] bg-black col-span-1">
-
+                            <div className="w-full min-h-[200px] col-span-2 py-4 flex justify-center md:col-span-1">
+                                <div className='w-full'>
+                                    <div className='w-full'>
+                                        <p className='font-roboto text-lg font-bold text-head-charleston-green'>THE RACE</p>
+                                    </div>
+                                    <nav className='w-full mt-3'>
+                                        <ul className='w-auto space-y-2'>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Race Details</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Event Registration</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Event Schedule</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Medals & Awards</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Buy Tickets</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Participant Guide</Link></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                                <div className='w-full'>
+                                    <div className='w-full'>
+                                        <p className='font-roboto text-lg font-bold text-head-charleston-green'>SUPPORT</p>
+                                    </div>
+                                    <nav className='w-full mt-3'>
+                                        <ul className='w-auto space-y-2'>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Contact Support</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>FAQ</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Race Regulations</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Safety Tips</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Volunteer Info</Link></li>
+                                            <li className='font-roboto text-base text-para-gray font-medium'><Link>Blog & News</Link></li>
+                                        </ul>
+                                    </nav>
+                                </div>
                             </div>
-                            <div className="w-full min-h-[300px] bg-black col-span-1">
-
+                            <div className="w-full min-h-[300px] bg-black col-span-2 md:col-span-1">
+                                
                             </div>
                         </div>
                     </div>
