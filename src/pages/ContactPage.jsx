c435cfbf42f2215e51e837c26293450c64f7d0e6
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-04 22:21:46 +0600

    contact page left side content done on 01/04/2025 at 10:21 pm

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdEmail, MdKeyboardArrowRight } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const ContactPage = () => {
     return (
@@ -38,8 +39,26 @@ const ContactPage = () => {
                     <div className="w-full max-w-[1120px] mx-auto px-4">
                         <div className="w-full min-h-[550px] mt-16 grid grid-cols-1 gap-10 lg:mt-26 lg:grid-cols-2">
                             <div className="w-full h-[390px] bg-contact-form-img bg-cover bg-center bg-no-repeat p-7 md:flex md:items-end md:justify-end md:h-[490px] lg:h-full">
-                                <div className="w-full h-full bg-white md:w-[350px] md:max-h-[210px]">
-
+                                <div className="w-full h-full bg-primary-yellow p-6 flex items-center justify-between flex-col gap-5 md:w-[350px] md:max-h-[210px]">
+                                    <div className="w-full flex flex-col items-center md:flex-row md:justify-between">
+                                        <div className="w-[55px] h-[55px] bg-head-charleston-green flex items-center justify-center">
+                                            <FaPhoneAlt className='text-lg text-primary-yellow'></FaPhoneAlt>
+                                        </div>
+                                        <div className="w-auto text-center mt-4 md:mt-0">
+                                            <h2 className="font-roboto text-base text-head-charleston-green font-black uppercase md:text-right">Phone</h2>
+                                            <p  className="font-roboto text-[22px] text-head-charleston-green font-medium">+60 12-345 6789</p>
+                                        </div>
+                                    </div>
+                                    <div className="w-full border-b border-head-charleston-green"></div>
+                                    <div className="w-full flex flex-col items-center md:flex-row md:justify-between">
+                                        <div className="w-[55px] h-[55px] bg-head-charleston-green flex items-center justify-center">
+                                            <MdEmail className='text-xl text-primary-yellow'></MdEmail>
+                                        </div>
+                                        <div className="w-auto text-center mt-4 md:mt-0">
+                                            <h2 className="font-roboto text-base text-head-charleston-green font-black uppercase md:text-right">Email</h2>
+                                            <p  className="font-roboto text-[22px] text-head-charleston-green font-medium">help@raceflow.com</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div className="w-full min-h-[490px] bg-pattern bg-cover bg-center bg-no-repeat lg:h-full">
