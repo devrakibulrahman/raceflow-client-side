4e172fce46296e430c5fad98bb3423e84ed098b3
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-05 10:27:09 +0600

    marathon image added in the tailwind.config file as a background image done on 01/05/2025 at 10:27 am

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ export default {
         "login": 'url(/img/login.png)',
         "register": 'url(/img/register.png)',
         "contact": 'url(/img/contact.png)',
+        "marathon": 'url(/img/marathon.png)',
         "contact-form-img": 'url(/img/contact-form-img.png)',
         "pattern": 'url(/pattern.png)',
       },
