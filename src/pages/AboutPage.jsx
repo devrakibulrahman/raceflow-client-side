c86730a9e39a17a3fe6c489eefbca9401a3143a3
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-05 16:51:06 +0600

    about page heading image added and heading text with navigation link added done on 01/05/2025 at 04:51 pm

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,24 @@
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const AboutPage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="w-full min-h-[500px] bg-about bg-center bg-cover bg-no-repeat flex items-center justify-center">
+                <div className="w-full">
+                    <div className="container mx-auto px-4">
+                        <div className="w-full text-center">
+                            <h1 className="font-roboto text-[40px] font-bold text-white uppercase transition-all ease-linear duration-200 md:text-[50px] xl:text-[70px]">About Us</h1>
+                            <div className="w-full flex items-center justify-center gap-2 mt-4">
+                                <div className="w-auto"><Link to='/' className="font-roboto text-sm text-white/80 font-normal transition ease-linear duration-200 hover:text-white">Home</Link></div>
+                                <div className="w-auto"><MdKeyboardArrowRight className="text-white"></MdKeyboardArrowRight></div>
+                                <div className="w-auto"><Link className="font-roboto text-sm text-white font-normal">About Us</Link></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
