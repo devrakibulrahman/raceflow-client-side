b4a504f8931675a178d643f8324a1e688e012214
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-05 17:29:59 +0600

    details page heading image added done on 01/05/2025 at 05:29 pm

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,8 +1,12 @@
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const DetailsPage = () => {
     return (
         <>
-            
+            <div className="w-full min-h-[500px] bg-details bg-center bg-cover bg-no-repeat flex items-center justify-center">
+                
+            </div>
         </>
     );
 };
