eb3e4acfcd72f2909552428ec8fbd5e15e426da6
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-05 17:30:58 +0600

    details page heading and link added done on 01/05/2025 at 05:30 pm

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -5,7 +5,18 @@ const DetailsPage = () => {
     return (
         <>
             <div className="w-full min-h-[500px] bg-details bg-center bg-cover bg-no-repeat flex items-center justify-center">
-                
+                <div className="w-full">
+                    <div className="container mx-auto px-4">
+                        <div className="w-full text-center">
+                            <h1 className="font-roboto text-[40px] font-bold text-white uppercase transition-all ease-linear duration-200 md:text-[50px] xl:text-[70px]">Marathon Details</h1>
+                            <div className="w-full flex items-center justify-center gap-2 mt-4">
+                                <div className="w-auto"><Link to='/' className="font-roboto text-sm text-white/80 font-normal transition ease-linear duration-200 hover:text-white">Home</Link></div>
+                                <div className="w-auto"><MdKeyboardArrowRight className="text-white"></MdKeyboardArrowRight></div>
+                                <div className="w-auto"><Link className="font-roboto text-sm text-white font-normal">Details</Link></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
