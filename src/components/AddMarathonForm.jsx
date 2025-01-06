133886ffc6cca815777c0d68156e667269d7f132
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-07 02:28:46 +0600

    added heading text and added image in the add marathon form done on 01/07/2025 at 02:28 am

--- src/components/AddMarathonForm.jsx
+++ src/components/AddMarathonForm.jsx
@@ -1,7 +1,7 @@
 
 const AddMarathonForm = () => {
     return (
-        <div>
+        <div className="w-full max-w-[650px] min-h-[550px] bg-pattern bg-cover bg-center bg-no-repeat">
             
         </div>
     );
