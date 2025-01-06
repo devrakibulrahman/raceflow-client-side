ecdf23e3542a3bd41516a9ef351c4c4a6bed698c
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-06 20:50:40 +0600

    import the dashboard sidebar component in the dashboard page component done on 01/06/2025 at 08:50 pm

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,10 +1,12 @@
 import DashboardNavbar from "../components/DashboardNavbar";
+import DashboardSidebar from "../components/DashboardSidebar";
 
 const DashboardPage = () => {
     return (
         <>
             <div className="w-full">
                 <DashboardNavbar></DashboardNavbar>
+                <DashboardSidebar></DashboardSidebar>
             </div>
         </>
     );
