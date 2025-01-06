c0b547182bfefc323e099d77467e0e8e3f32a226
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-06 16:28:07 +0600

    import registration form component in the registration page done on 01/06/2025 at 04:28 pm

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,9 +1,59 @@
 
 const RegistrationForm = () => {
+
+    // form submit function declare here ----> 
+    // const handleFormSubmit = (e) => {
+    //     e.preventDefault();
+    // };
+
     return (
-        <div>
-            
-        </div>
+        <>
+            {/* <form onSubmit={handleFormSubmit} className="w-full">
+                <div className="w-full grid grid-cols-1 gap-4">
+                    <div className="w-full flex flex-col items-center justify-center gap-4 md:flex-row">
+                        <div className="w-full">
+                            <div className="w-full mb-3">
+                                <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug">Name</label>
+                            </div>
+                            <div className="w-full">
+                                <input type="text" placeholder="full name" name="name" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
+                            </div>
+                        </div>
+                        <div className="w-full">
+                            <div className="w-full mb-3">
+                                <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug">Photo Url</label>
+                            </div>
+                            <div className="w-full">
+                                <input type="text" placeholder="photo url" name="photo" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full space-y-4">
+                        <div className="w-full">
+                            <div className="w-full mb-3">
+                                <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug">Email</label>
+                            </div>
+                            <div className="w-full">
+                                <input type="text" placeholder="example@gmail.com" name="email" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full">
+                        <div className="w-full py-2 mb-3">
+                            <div className="w-auto flex items-center gap-2">
+                                <input type="checkbox" />
+                                <p className="font-roboto text-head-charleston-green text-base font-medium ">Accept Term & Condition</p>
+                            </div>
+                        </div>
+                        <div className="w-full">
+                            <button className="w-full p-4 border border-head-charleston-green bg-white flex items-center justify-center gap-3 transition ease-linear duration-200 group hover:bg-head-charleston-green">
+                                <span className="font-roboto text-base text-head-charleston-green font-bold uppercase group-hover:text-primary-yellow">Register</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form> */}
+        </>
     );
 };
 
