9d59e4eeca1cd1c7ca3771a5398498e7314daa0f
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-06 15:43:37 +0600

    heading text and link added in the registration page done on 01/06/2025 at 03:43 pm

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -5,18 +5,20 @@ const RegistrationPage = () => {
     return (
         <>
             <div className="w-full min-h-[500px] bg-marathon-registration bg-center bg-cover bg-no-repeat flex items-center justify-center">
-                {/* <div className="w-full">
+                <div className="w-full">
                     <div className="container mx-auto px-4">
                         <div className="w-full text-center">
-                            <h1 className="font-roboto text-[40px] font-bold text-white uppercase transition-all ease-linear duration-200 md:text-[50px] xl:text-[70px]">Contact Us</h1>
+                            <h1 className="font-roboto text-[40px] font-bold text-white uppercase transition-all ease-linear duration-200 md:text-[50px] xl:text-[70px]">Marathon Registration</h1>
                             <div className="w-full flex items-center justify-center gap-2 mt-4">
                                 <div className="w-auto"><Link to='/' className="font-roboto text-sm text-white/80 font-normal transition ease-linear duration-200 hover:text-white">Home</Link></div>
+                                <div className="w-auto"><MdKeyboardArrowRight className="text-white"></MdKeyboardArrowRight></div>
+                                <div className="w-auto"><Link className="font-roboto text-sm text-white/80 font-normal transition ease-linear duration-200 hover:text-white">Details</Link></div>
                                 <div className="w-auto"><MdKeyboardArrowRight className="text-white"></MdKeyboardArrowRight></div>
                                 <div className="w-auto"><Link className="font-roboto text-sm text-white font-normal">Contact</Link></div>
                             </div>
                         </div>
                     </div>
-                </div> */}
+                </div>
             </div>
         </>
     );
