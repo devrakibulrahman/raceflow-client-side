cb5fde1dfaf025c845dc980d88a8cb8bcd962353
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-06 16:03:03 +0600

    registration page form heading design done on 01/06/2025 at 04:02 pm

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -23,14 +23,16 @@ const RegistrationPage = () => {
             <div className="w-full min-h-[500px] py-16 lg:py-24">
                 <div className="w-full">
                     <div className="container mx-auto px-4">
-                        <div className="w-full text-center">
-                            <div className="w-full mb-5">
-                                <h3 className="font-roboto text-sm font-medium text-para-gray uppercase">Register Now</h3>
-                            </div>
-                            <div className="w-full flex items-center justify-center flex-col">
-                                <h1 className="font-roboto font-bold text-[32px] text-head-charleston-green transition-all ease-linear duration-200 md:text-[38px] lg:text-[50px]">SUBMIT YOUR EVENT <span className="bg-primary-yellow">REGISTRATION</span> FORM.</h1>
-                                <div className="w-full max-w-[700px] mt-5">
-                                    <p className="font-roboto text-xl font-normal text-para-gray leading-relaxed transition-all ease-linear duration-200 md:text-[22px]">Fill out the form below to confirm your participation in the HorizonRun Challenge.</p>
+                        <div className="w-full min-h-[400px] bg-slate-50">
+                            <div className="w-full min-h-[400px] text-center p-10 bg-pattern bg-cover bg-center bg-no-repeat lg:p-12">
+                                <div className="w-full mb-5">
+                                    <h3 className="font-roboto text-sm font-medium text-para-gray uppercase">Register Now</h3>
+                                </div>
+                                <div className="w-full flex items-center justify-center flex-col">
+                                    <h1 className="font-roboto font-bold text-[32px] text-head-charleston-green transition-all ease-linear duration-200 md:text-[38px] lg:text-[50px]">SUBMIT YOUR EVENT <span className="bg-primary-yellow">REGISTRATION</span> FORM.</h1>
+                                    <div className="w-full max-w-[700px] mt-5">
+                                        <p className="font-roboto text-xl font-normal text-para-gray leading-relaxed transition-all ease-linear duration-200 md:text-[22px]">Join the HorizonRun Challenge by completing the form below.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
