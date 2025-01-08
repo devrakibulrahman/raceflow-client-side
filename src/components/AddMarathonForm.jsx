8239499c73540942ebfdb188f8b6776b652fa063
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-09 00:35:14 +0600

    install the format from date.fns and get the form values done on 01/09/2025 at 12:35 am

--- src/components/AddMarathonForm.jsx
+++ src/components/AddMarathonForm.jsx
@@ -1,17 +1,51 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { format } from "date-fns";
+import { AuthContext } from "../contexts/AuthProvider";
 import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker from "react-datepicker";
 
 const AddMarathonForm = () => {
+
+    const {user} = useContext(AuthContext);
+
     const [startDate, setStartDate] = useState(null);
+    const [endData, setEndDate] = useState(null);
+    const [maraStartDate, setMaraStartDate] = useState(null);
     const [option, setOption] = useState('Select Running Distance');
     const [selectActive, setSelectActive] = useState(false);
 
-    const options = [ '3k', '10k', '25k' ]; 
+    const options = [ '3k', '10k', '25k' ];
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
+
+        const form = new FormData(e.target);
+        const marathonTitle = form.get('title');
+        const marathonImage =  form.get('photo');
+        const registrationStartDate = format(new Date(startDate), 'P');
+        const registrationEndDate = format(new Date(endData), 'P');
+        const marathonStartDate = format(new Date(maraStartDate), 'P');
+        const location = form.get('location');
+        const runningDistance = option === 'Select Running Distance' ? '' : option;
+        const description = form.get('message');
+
+        const AddMarathon = {
+            marathonTitle,
+            marathonImage,
+            registrationStartDate,
+            registrationEndDate,
+            marathonStartDate,
+            location,
+            runningDistance,
+            description,
+            regCount: 0,
+            username: user?.displayName,
+            email: user?.email
+        };
+
+        console.log(AddMarathon);
+
     };
 
     return (
@@ -24,7 +58,7 @@ const AddMarathonForm = () => {
                                 <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug uppercase">Marathon Title</label>
                             </div>
                             <div className="w-full">
-                                <input type="text" placeholder="First Name" name="fname" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
+                                <input type="text" placeholder="Marathon Title" name="title" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
                             </div>
                         </div>
                         <div className="w-full">
@@ -32,7 +66,7 @@ const AddMarathonForm = () => {
                                 <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug uppercase">Marathon Image</label>
                             </div>
                             <div className="w-full">
-                                <input type="text" placeholder="Image Url" name="fname" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
+                                <input type="text" placeholder="Image Url" name="photo" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
                             </div>
                         </div>
                     </div>
@@ -50,7 +84,7 @@ const AddMarathonForm = () => {
                                 <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug uppercase">End Registration Date</label>
                             </div>
                             <div className="w-full min-h-[58px] bg-white border border-slate-200 cursor-text transition ease-linear duration-200 hover:border-head-charleston-green">
-                                <DatePicker selected={startDate} onChange={(date) => {setStartDate(date)}} disabledKeyboardNavigation placeholderText="Registration End Date" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"></DatePicker>
+                                <DatePicker selected={endData} onChange={(date) => {setEndDate(date)}} disabledKeyboardNavigation placeholderText="Registration End Date" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"></DatePicker>
                             </div>
                         </div>
                     </div>
@@ -60,7 +94,7 @@ const AddMarathonForm = () => {
                                 <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug uppercase">Marathon Start Date</label>
                             </div>
                             <div className="w-full min-h-[58px] bg-white border border-slate-200 cursor-text transition ease-linear duration-200 hover:border-head-charleston-green">
-                                <DatePicker selected={startDate} onChange={(date) => {setStartDate(date)}} disabledKeyboardNavigation placeholderText="Marathon Start Date" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"></DatePicker>
+                                <DatePicker selected={maraStartDate} onChange={(date) => {setMaraStartDate(date)}} disabledKeyboardNavigation placeholderText="Marathon Start Date" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"></DatePicker>
                             </div>
                         </div>
                     </div>
@@ -70,7 +104,7 @@ const AddMarathonForm = () => {
                                 <label className="font-roboto text-base text-head-charleston-green font-bold leading-snug uppercase">Location</label>
                             </div>
                             <div className="w-full">
-                                <input type="text" placeholder="Location" name="email" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
+                                <input type="text" placeholder="Location" name="location" autoComplete="off" className="w-full p-4 font-roboto font-normal text-base text-head-charleston-green bg-white border border-slate-200 transition ease-linear duration-200 hover:border-head-charleston-green focus:outline-none placeholder:text-[#A1A1AA] placeholder:font-light"/>
                             </div>
                         </div>
                         <div className="w-full">
